dfc71b6b5d85aeb665de08b1e7c634ae67fe4166
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2024-03-24 10:37:13 +0530

    Time: 292 ms (5.02%) | Memory: 105.8 MB (6.40%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -13,5 +13,15 @@ public:
             return it.first;
         }
         return -1;
+
+        // int xor1=0;
+        // int xor2 = 0;
+        // for(int i =1; i<=n; i++) {
+        //     xor1 = xor1^nums[i-1];
+        //     xor2 = xor2^i;
+        // }
+
+        // int ans = xor1 ^ xor2;
+        // return ans;
     }
 };
