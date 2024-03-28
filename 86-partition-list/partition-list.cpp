3ebd5ad2790394ede762403bb92527fef2b97369
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2024-03-28 17:42:04 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (92.63%) - LeetSync

--- 86-partition-list/partition-list.cpp
+++ 86-partition-list/partition-list.cpp
@@ -11,75 +11,37 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-         if (head == nullptr || head->next == nullptr)
-            return head;
+           if(head == nullptr || head -> next == nullptr)
+           return head;
+           
+           ListNode *smallerHead = new ListNode();//dummy head for smaller values
+           ListNode *smallerTail = smallerHead;
+           ListNode *largerHead = new ListNode();//dummy head for larger values
+           ListNode *largerTail = largerHead;
 
-        ListNode* smallerHead = new ListNode(); // Dummy head for smaller values
-        ListNode* smallerTail = smallerHead;
-        ListNode* largerHead = new ListNode(); // Dummy head for larger values
-        ListNode* largerTail = largerHead;
-
-        ListNode* curr = head;
-        while (curr != nullptr) {
-            if (curr->val < x) {
-                smallerTail->next = curr;
+           ListNode *curr = head;
+           while(curr) {
+            if(curr->val < x) {
+                smallerTail -> next = curr;
                 smallerTail = smallerTail->next;
-            } else {
-                largerTail->next = curr;
+            }else{
+                largerTail -> next = curr;
                 largerTail = largerTail->next;
             }
             curr = curr->next;
-        }
+           }
+           
+           //connect the 2 LL
+           smallerTail->next = largerHead->next;
+           largerTail->next = NULL;
+       
+           //Update Head
+           head = smallerHead->next;
 
-        // Connect the two partitions
-        smallerTail->next = largerHead->next;
-        largerTail->next = nullptr;
+           //Free the dummy Heads
+           delete smallerHead;
+           delete largerHead;
 
-        // Update head
-        head = smallerHead->next;
-
-        // Free the dummy heads
-        delete smallerHead;
-        delete largerHead;
-
-        return head;
-    }
-    //     if(head == NULL || head->next==NULL)
-    //     return head;
-    
-    //    ListNode* curr = head;
-    //    ListNode *smallerhead = NULL;
-    //    ListNode *largerhead = NULL;
-    //    ListNode *head1 = NULL, *head2 =NULL;
-    //    while(curr){
-    //     if(curr->val < x){
-    //             if(smallerhead==NULL){
-    //               smallerhead = curr;
-    //                head1 = smallerhead;
-    //             }
-    //             else{
-    //                 smallerhead->next = curr;
-    //                 smallerhead = smallerhead->next;
-    //                 curr=curr->next;
-    //             } 
-    //     }else{
-    //          if(largerhead==NULL){
-    //               largerhead = curr;
-    //                head2 = largerhead;
-    //          }
-    //             else{
-    //                 largerhead->next = curr;
-    //                 largerhead = largerhead->next;
-    //                 curr=curr->next;
-    //             } 
-    //     }
-    //    }
-    //    //connect both the Linked Lists
-    //    smallerhead->next = head2;
-    //    largerhead->next=NULL;
-    //    delete smallerhead;
-    //    delete largerhead;
-    //    delete head2;
-    //    return head1;
-    // }
+            return head;
+             }
 };
