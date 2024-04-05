d56f27e06756668bfe9bee70f7871d99bb5517e0
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2024-04-05 14:47:47 +0530

    Time: 4 ms (42.41%) | Memory: 7.7 MB (74.45%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.cpp
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -2,6 +2,11 @@ class Solution {
 public:
     string makeGood(string s) {
         string res = "";
+        //it handles cases where more than two adjacent characters need to be removed and
+        // it may be present before and after a bad combo.
+        //eg "abBAcC"
+        //bB is the bad part , after removing ,
+        // we need to check the resultant string that is there any formation of a bad combo from last char and current char
         for(int i = 0; i < s.length(); i++) {
              if(res.empty() == false && abs(res.back() -s[i])==32)
                    res.pop_back();
