285eaf41b34486e55a067cefbd19274820de6765
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2024-04-07 21:30:07 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (11.83%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -14,7 +14,8 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(p==nullptr || q==nullptr)
              return (p==q);
-
+        
+        //apply any traversal - here preorder applied
         return (p->val == q->val &&
                isSameTree(p->left, q->left) &&
                isSameTree(p->right,q->right));
