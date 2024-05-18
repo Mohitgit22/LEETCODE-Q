845ffe3fadf41c06dc1ac6b9ac5c83203c9f373c
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2024-05-18 23:25:08 +0530

    Time: 3 ms (60.76%) | Memory: 13.6 MB (35.35%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,26 +1,58 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int low = 0;
-        int high = nums.size() -1;
-        while(low <= high) {
-            int mid = low + (high - low) / 2;
-            if(nums[mid] == target)
-            return mid;
+
+    int minindex(vector<int>& nums){
+        int n = nums.size();
+        int s = 0;
+        int e = n-1;
+        int index = -1;
+        while( s<= e) {
+            if(nums[s] < nums[e])
+            return s;
             
-            //left part is sorted
-            if(nums[low] <= nums[mid]){
-                if(nums[low] <= target && target < nums[mid])
-                high = mid - 1;
-                else
-                low = mid + 1;
-            }else{ // right part is sorted
-                if(nums[mid] <= target && target <= nums[high] )
-                low = mid + 1;
-                else
-                high = mid - 1;
+            int mid = s + (e - s) / 2;
+            int next = (mid + 1) % n;
+            int prev = (mid + n -1) % n;
+            if(nums[mid] <= nums[next] && nums[mid] <= nums[prev]){
+                return mid;
             }
+            else if(nums[s] <= nums[mid])
+                s = mid + 1;
+            else 
+              e = mid -1;
+            }
+            return index;
         }
-         return -1;
+
+    int BS(vector<int>& nums,int start, int end, int target){
+        int s = start;
+        int e = end;
+        while(s <= e) {
+            int mid = s + (e-s)/2;
+            if(nums[mid] == target)
+             return mid;
+            else if(nums[mid] < target)
+              s = mid + 1;
+            else 
+              e = mid - 1;
+        }
+        return -1;
+    }
+
+    int search(vector<int>& nums, int target) {
+
+       int n = nums.size() ;
+       int index = minindex(nums);
+    //    cout<<"index is "<<index<<endl;
+       int left = BS(nums, 0, index - 1, target);
+       int right = BS(nums, index, n-1, target);
+
+    //    cout<<left <<" hi"<<right<<endl;
+       if(left == -1 && right == -1)
+        return -1;
+       else if(left != -1)
+        return left;
+        else
+        return right;
     }
 };
