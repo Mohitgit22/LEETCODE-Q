c690f2f87ffb44a16d6e1c9cb2fd66bdc7142e0d
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2024-06-07 16:31:45 +0530

    Time: 117 ms (20.59%) | Memory: 54.5 MB (22.49%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,53 +1,86 @@
 class Solution {
 public:
-  vector<int> parent;
-    vector<int> rank;
+ void dfs( vector<vector<int>>& adj, int u, vector<bool>& vis) {
+          vis[u] = true;
 
-    int find(int x) {
-        if(x == parent[x])
-           return x;
-        
-        return parent[x] = find(parent[x]);
+          for(int& v: adj[u]) {
+            if(!vis[v])
+                    dfs(adj, v, vis);
+          }
+ }
+ int makeConnected(int n, vector<vector<int>>& connections) {
+    vector<bool> vis(n, false);
+
+    if(n-1 > connections.size())
+      return -1;
+
+    vector<vector<int>> adj(n);
+
+    for(int i = 0; i < connections.size(); i++) {
+        adj[connections[i][0]].push_back(connections[i][1]);
+        adj[connections[i][1]].push_back(connections[i][0]);
+    }
+   
+    int cnt = 0; 
+    for(int i = 0; i < n; i++) {
+        if(!vis[i]) {
+            dfs(adj, i, vis);
+            cnt++;
+        }
     }
 
-    void union_dsu(int x, int y) {
-        int x_parent = find(x);
-        int y_parent = find(y);
+    return cnt - 1;
+ }
+
+};
+//   vector<int> parent;
+//     vector<int> rank;
+
+//     int find(int x) {
+//         if(x == parent[x])
+//            return x;
+        
+//         return parent[x] = find(parent[x]);
+//     }
+
+//     void union_dsu(int x, int y) {
+//         int x_parent = find(x);
+//         int y_parent = find(y);
        
         
-        if(rank[x_parent] > rank[y_parent]) 
-           parent[y_parent] = x_parent;
-        else if(rank[y_parent] > rank[x_parent])
-           parent[x_parent] = y_parent;
-        else{
-            parent[x_parent] = y_parent;
-            rank[y_parent]+= 1;
-        }
-    }
-    int makeConnected(int n, vector<vector<int>>& connections) {
-        parent.resize(n);
-        rank.resize(n, 0);
+//         if(rank[x_parent] > rank[y_parent]) 
+//            parent[y_parent] = x_parent;
+//         else if(rank[y_parent] > rank[x_parent])
+//            parent[x_parent] = y_parent;
+//         else{
+//             parent[x_parent] = y_parent;
+//             rank[y_parent]+= 1;
+//         }
+//     }
+//     int makeConnected(int n, vector<vector<int>>& connections) {
+//         parent.resize(n);
+//         rank.resize(n, 0);
 
-        for(int i = 0; i < n; i++) {
-            parent[i] = i;
-        }
-         if(n-1 > connections.size() )
-            return -1;
+//         for(int i = 0; i < n; i++) {
+//             parent[i] = i;
+//         }
+//          if(n-1 > connections.size() )
+//             return -1;
 
-        int components = n;
-        for(auto &conn : connections) {
-            int x = conn[0];
-            int y = conn[1];
+//         int components = n;
+//         for(auto &conn : connections) {
+//             int x = conn[0];
+//             int y = conn[1];
 
-            int x_parent = find(x);
-            int y_parent = find(y);
+//             int x_parent = find(x);
+//             int y_parent = find(y);
 
-            if(x_parent != y_parent){
-                union_dsu(x, y);
-                components -= 1;
-            }
-        }
+//             if(x_parent != y_parent){
+//                 union_dsu(x, y);
+//                 components -= 1;
+//             }
+//         }
 
-        return components-1;
-    }
-};
+//         return components-1;
+//     }
+// };
