6c5ab91b253c37261e00ef77de1774925bacfacc
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2024-06-10 21:29:07 +0530

    Time: 65 ms (36.38%) | Memory: 17.8 MB (79.44%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -1,42 +1,132 @@
+// class Solution {
+// public:
+//     vector<int> parent;
+//     vector<int> rank;
+
+//     int find(int x) {
+//         if(x == parent[x])
+//          return x;
+
+//         return parent[x] = find(parent[x]);
+//     }
+
+//     void Union(int x , int y) {
+        
+//         int x_parent = find(x);
+//         int y_parent = find(x);
+
+//         if(x_parent != y_parent) {
+//             if(rank[x_parent] > rank[y_parent])
+//              parent[y_parent] = x_parent;
+//             else if(rank[x_parent] < rank[y_parent])
+//              parent[x_parent] = y_parent;
+//              else{
+//              parent[y_parent] = x_parent;  
+//              rank[x_parent]++;
+//              }
+//         }
+//     }
+
+//     bool check(vector<int> vec1, vector<int> vec2){
+//         int x1 = vec1[0];
+//         int y1 = vec1[1];
+
+//         int x2 = vec2[0];
+//         int y2 = vec2[1];
+
+//         if(x1 == x2) 
+//          return true;
+//         if(y1 == y2)
+//          return true;
+        
+//         return false;
+//     }
+
+//     int removeStones(vector<vector<int>>& stones) {
+//         int n = stones.size();
+//         parent.resize(n);
+//         rank.resize(n, 0);
+
+//         for(int i = 0; i < n; i++) {
+//             parent[i] = i;
+//         }
+       
+//         int cnt = 0;
+//         for(int i = 1; i < n; i++) {
+
+//             // for(int j = i; j < v; j++) {
+//                 if(check(stones[i], stones[i-1]) == true){
+//                     cnt++;
+//                 }   
+//                     Union(stones[i][0], stones[i-1][0]);
+//                     Union(stones[i][1], stones[i-1][1]);  
+//             // }
+//         }
+//         return  cnt;
+//     }
+// };
+
+
 class Solution {
 public:
-    bool isValid(vector<int>&pos1, vector<int>&pos2)
-    {
-        if (pos1[0] == pos2[0]) return true;
-        if (pos1[1] == pos2[1]) return true;
-        return false;
+    vector<int> parent;
+    vector<int> rank;
+
+    int find(int x) {
+        if (x == parent[x])
+            return x;
+        return parent[x] = find(parent[x]);
     }
-    int findParent(vector<int>&parent, int node)
-    {
-        while(parent[node] != node) { node = parent[node]; }
-        return node;
-    }
-    void merge(vector<int>&parent, int& componentCount, int node1, int node2)
-    {
-        int parent1 = findParent(parent, node1);
-        int parent2 = findParent(parent, node2);
-        if (parent1 != parent2) 
-        {
-            componentCount--; //whenever we merge two components, totalComponentCount decrease by 1
-            parent[parent1] = parent2; //or parent[parent2] = parent1 would also work :)
-        }
-    }
-    int removeStones(vector<vector<int>>& stones)
-    {
-        int n = stones.size();
-        vector<int>parent(n);
-        for (int i = 0; i < n; i++) parent[i] = i;
-        //==============================================
-        int componentCount = n; //assuming "n" independent components at start
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = i; j < n; j++)
-            {
-                if (isValid(stones[i], stones[j]))
-                    merge(parent, componentCount, i, j);
+
+    void Union(int x, int y) {
+        int x_parent = find(x);
+        int y_parent = find(y);
+
+        if (x_parent != y_parent) {
+            if (rank[x_parent] > rank[y_parent])
+                parent[y_parent] = x_parent;
+            else if (rank[x_parent] < rank[y_parent])
+                parent[x_parent] = y_parent;
+            else {
+                parent[y_parent] = x_parent;
+                rank[x_parent]++;
             }
         }
-        //===============================================
-        return (n - componentCount); 
+    }
+
+    bool check(vector<int>& vec1, vector<int>& vec2) {
+        int x1 = vec1[0];
+        int y1 = vec1[1];
+        int x2 = vec2[0];
+        int y2 = vec2[1];
+
+        return (x1 == x2 || y1 == y2);
+    }
+
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+        parent.resize(n);
+        rank.resize(n, 0);
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (check(stones[i], stones[j])) {
+                    Union(i, j);
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (find(i) == i) {
+                cnt++;
+            }
+        }
+
+        return n - cnt;
     }
 };
