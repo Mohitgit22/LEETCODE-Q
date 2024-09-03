584247936fd7042b946dfcdc7e2f06aab87beb55
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2024-09-03 18:47:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (11.04%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,63 +1,24 @@
 class Solution {
 public:
-    // eg [2.1.1.2]
-    //o/p : 4
+    int f(int ind, vector<int>& nums, vector<int>& dp) {
+        if(ind == 0)
+        return nums[0];
 
-    // recursive approach -- not applicable
-    // int solve(vector<int>& nums, int i) {
-    //     if(i >= nums.size()) 
-    //         return 0;
+        if(ind < 0)
+        return 0;
 
-    //     int steal = nums[i] + solve(nums, i+2);
-    //     int skip = solve(nums, i+1);
+        if(dp[ind] != -1)
+        return dp[ind];
 
-    //     return max(steal, skip);
-    //     }
+        int pick = nums[ind] + f(ind-2, nums, dp);
+        int notpick = f(ind - 1, nums, dp);
+
+        return dp[ind] = max(pick, notpick);
+    }
     
-    // int rob(vector<int>& nums) {
-    //     int n = nums.size();
-    //     return solve(nums, 0);
-    // }
-
-   // using memoization
-//    int t[101];
-//    int solve(vector<int>& nums, int i, int n) {
-//         if(i >= n)
-//         return 0;
-
-//         if(t[i]!=-1)
-//         return t[i];
-
-//        int steal = nums[i] + solve(nums, i+2, n);
-//        int skip = solve(nums, i+1, n);
-//         return t[i] = max(steal, skip);
-//    }
-
-//      int rob(vector<int>& nums) {
-//         int n = nums.size();
-//         memset(t, -1, sizeof(t));
-//         return solve(nums, 0,n);
-//     }  
-
-
-
-
-
-   //using bottom up approach
-
     int rob(vector<int>& nums) {
-         int n = nums.size();
-         int t[n+1];
-
-         t[0] = 0;
-         t[1] = nums[0];
-
-         for(int i = 2; i<=n; i++) {
-            int steal = nums[i-1] + t[i-2];
-            int skip = t[i-1];
-
-            t[i] = max(skip, steal);
-         }
-         return t[n];
-    }  
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        return f(n-1, nums, dp);
+    }
 };
