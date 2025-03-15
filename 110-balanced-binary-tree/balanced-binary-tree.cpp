2ef1a5b7c8bfe1ca941523ca033cf98dad1c5fae
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-03-15 23:55:31 +0530

    Time: 4 ms (11.60%) | Memory: 23.1 MB (24.05%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -11,45 +11,26 @@
  */
 class Solution {
 public:
-
-    int height(TreeNode* root) {
+    int height(TreeNode* root)  {
         if(root == nullptr)
         return 0;
 
-        return 1+max(height(root->left), height(root->right));
+        return 1 + max(height(root->left), height(root->right));
     }
-
-    void inorder(TreeNode* root, vector<TreeNode*> &vec) {
-       if(root == nullptr)
-       return ;
-
-       inorder(root->left, vec);
-       vec.push_back(root);
-       inorder(root->right, vec);
-    }
-    
     bool isBalanced(TreeNode* root) {
         if(root == nullptr)
         return true;
 
-        vector<TreeNode*> vec;
+        int lh = height(root->left);
+        int rh = height(root->right);
 
-        inorder(root, vec);
-        int  height_left_subtree;
-        int  height_right_subtree;
-        for(int i = 0; i < vec.size(); i++) {
-            TreeNode* curr = vec[i];
-            if(curr->left)
-            height_left_subtree = height(curr->left);
-            if(curr->right)
-            height_right_subtree = height(curr->right);
-            if(abs(height_left_subtree - height_right_subtree)> 1)
-            return false;
-     
-           height_left_subtree = 0;
-           height_right_subtree = 0;
+        if(abs(lh - rh) > 1) return false;
+        
+        bool left = isBalanced(root->left);
+        bool right = isBalanced(root->right);
 
-        }
+        if(left == false || right == false)
+        return false;
 
         return true;
     }
