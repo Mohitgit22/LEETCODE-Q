5e6a3eb7356f2cb73538b18b9257ef880969f2ae
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-03-16 00:37:33 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (14.71%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -11,13 +11,22 @@
  */
 class Solution {
 public:
+    bool equal(TreeNode* node1, TreeNode* node2) {
+        if (node1 == nullptr && node2 == nullptr) return true;
+
+        if((node1 == nullptr and node2!= nullptr) || (node1 != nullptr and node2 == nullptr))
+         return false;
+
+         return (node1->val == node2->val);
+    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p==nullptr || q==nullptr)
-             return (p==q);
-        
-        //apply any traversal - here preorder applied
-        return (p->val == q->val &&
-               isSameTree(p->left, q->left) &&
-               isSameTree(p->right,q->right));
+        if(p == nullptr and q == nullptr)
+        return true;
+
+        bool curr_noderesult = equal(p, q);
+        if(curr_noderesult == false)
+        return false;
+
+       return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
