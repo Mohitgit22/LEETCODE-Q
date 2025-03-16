1b438c1b1e67eae7689d926f3a9037511d399783
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-03-16 21:20:33 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (87.92%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,31 +12,33 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-          queue<pair<TreeNode*, pair<int,int>>> q;
-          map<int, map<int, multiset<int>>> nodes;
-          q.push({root,{0, 0}});
-          while(!q.empty()) {
-            auto p = q.front();
-            q.pop();
-            TreeNode* node = p.first;
-            int x = p.second.first, y = p.second.second;
-            nodes[x][y].insert(node -> val);
-            if(node->left){
-                q.push({node->left, {x-1, y+1}});
-            }
-            if(node->right) {
-                q.push({node->right, {x+1, y+1}});
-            }
-          }
+        map<int, map<int, multiset<int>>> mp;
+        queue<pair<TreeNode*, pair<int , int>>> que;
+        que.push({root, {0,0}});
 
-          vector<vector<int>> ans;
-          for(auto a:nodes) {
-            vector<int> col;
-            for(auto q: a.second) {
-                col.insert(col.end(), q.second.begin(), q.second.end());
-            }
-            ans.push_back(col);
-          }
-          return ans;
+        while(!que.empty()) {
+            auto p = que.front();
+
+            TreeNode* node = p.first;
+            int x = p.second.first;
+            int y = p.second.second;
+            mp[x][y].insert(node->val);
+
+            que.pop();
+            if(node->left) que.push({node->left, {x-1, y+1}});
+            if(node->right) que.push({node->right, {x+1, y+1}});
+        }
+
+        vector<vector<int>> ans;
+        for(auto& [x_axis, levels] : mp) {
+             vector<int>col;
+           for(auto& [lvl, nodes]: levels){
+              for(auto it : nodes){
+                col.push_back(it);
+              }
+           }
+           ans.push_back(col);
+        }
+      return ans;
     }
 };
