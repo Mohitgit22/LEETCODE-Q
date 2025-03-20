ac728809237bc7a07ec3aae416eb802139acdb43
Mohit Verma <123758112+Mohitgit22@users.noreply.github.com>
2025-03-20 19:08:34 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (29.25%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -4,23 +4,24 @@ public:
     // int mincost(vector<int>& cost, int n) {
     //     if(n == 0 || n == 1) 
     //     return 0;
-
-    //     return min(cost[n-1] + mincost(cost, n-1) , 
-    //                cost[n-2] + mincost(cost, n-2));
+           
+        //    int onestep = cost[n-1] + mincost(cost, n-1);
+        //    int twostep = cost[n-2] + mincost(cost, n-2);
+    //     return min(onestep, twostep);
     // }
   
      //using top down approach
-     int mincost(vector<int>& cost, int n, vector<int>& dp) {
-        if(n == 0 || n==1)
-        return 0;
+    //  int mincost(vector<int>& cost, int n, vector<int>& dp) {
+    //     if(n == 0 || n==1)
+    //     return 0;
 
-        if(dp[n] == -1){
-            dp[n] = min(cost[n-1] + mincost(cost, n-1, dp) , 
-                       cost[n-2] + mincost(cost, n-2, dp));
-        }
+    //     if(dp[n] == -1){
+    //         dp[n] = min(cost[n-1] + mincost(cost, n-1, dp) , 
+    //                    cost[n-2] + mincost(cost, n-2, dp));
+    //     }
 
-        return dp[n];
-     }
+    //     return dp[n];
+    //  }
 
 
     int minCostClimbingStairs(vector<int>& cost) {
@@ -28,6 +29,16 @@ public:
         vector<int> dp(n+1, -1);
        dp[0] = 0;
        dp[1] = 0;
-       return mincost(cost, n, dp);
+
+
+       //bottom up
+       for(int state = 2; state <= n ; state++) {
+        int onestep = cost[state - 1] + dp[state - 1];
+        int twostep = cost[state - 2] + dp[state - 2];
+        dp[state] = min(onestep, twostep);
+       }
+
+    //    return mincost(cost, n, dp);
+    return dp[n];
     }
 };
